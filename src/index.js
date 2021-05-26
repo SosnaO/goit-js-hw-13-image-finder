491595fd8001529281200ff376634b111e931d41
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-05-26 22:04:28 +0300

    l

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './sass/main.scss';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  container: document.querySelector('.articles'),
+
+
+};
+  console.log(refs.searchForm)
+console.log(refs.container)
+
+refs.searchForm.addEventListener('submit', onSearch);
+function onSearch(event) {
+  event.preventDefault();
+
+};
