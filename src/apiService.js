4990907e4fa9959e0890fdf9f9c49a0c97e60833
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-05-27 19:05:54 +0300

    1

--- src/apiService.js
+++ src/apiService.js
@@ -7,7 +7,7 @@
 // };
 // const url ='https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12
 
-fetch('https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12&key=21803950-62f4c86011510fd15fe85c0d2')
- //fetch(url,options)
-.then(response => response.json())
-  .then(console.log())
+// fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${search}&page=1&per_page=12&key=21803950-62f4c86011510fd15fe85c0d2`)
+//  //fetch(url,options)
+// .then(response => response.json())
+//   .then(console.log())
--- src/index.js
+++ src/index.js
@@ -9,8 +9,23 @@ const refs = {
   console.log(refs.searchForm)
 console.log(refs.container)
 
+
+
+
 refs.searchForm.addEventListener('submit', onSearch);
 function onSearch(event) {
   event.preventDefault();
+  const searchName = event.currentTarget.elements.query.value;
+  console.log(searchName);
+
+
+
+fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchName}&page=1&per_page=12&key=21803950-62f4c86011510fd15fe85c0d2`)
+ //fetch(url,options)
+
+
+.then(response => response.json())
+  .then(console.log())
+
 
 };
