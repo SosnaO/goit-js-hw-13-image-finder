00893a8797e17cd48d0a3466054385a09e853cc3
SosnaO <solivijasosna@gmail.com>
2021-06-01 17:34:27 +0300

    убрала кнопку, когда Б12 картинок

--- src/apiService.js
+++ src/apiService.js
@@ -3,15 +3,26 @@ export default class NewsApiService {
   constructor() {
     this.searchName = '';
     this.page = 1;
-   }
-  fetchArticles() {
-      return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchName}&page=${this.page}&per_page=12&key=21803950-62f4c86011510fd15fe85c0d2`)
-      .then(response => response.json())
-        .then(data => {
-          this.incrementPage();
-          return data.hits;
-        });
   }
+  fetchArticles() {
+    return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchName}&page=${this.page}&per_page=12&key=21803950-62f4c86011510fd15fe85c0d2`)
+      .then(response => response.json())
+      .then(data => {
+        // if(data.hits<12) {
+        //  show();
+        // }
+        this.incrementPage();
+        return data.hits;
+      });
+    //.catch(error => console.log(error));
+
+
+  }
+  // if(data.hits<12) {
+  //   return
+  //  }
+
+
 
   incrementPage() {
     this.page += 1;
--- src/index.js
+++ src/index.js
@@ -11,9 +11,11 @@ const refs = {
   searchForm: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.btn-primary'),
-  fotoCard:document.querySelector('.foto-card'),
+  galleryImg: document.querySelector('.photo-card'),
+
 
 };
+//console.log(refs.galleryImg);
 const newApiService = new NewsApiService();
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
@@ -22,19 +24,33 @@ hide()
 function onSearch(event) {
   event.preventDefault();
   newApiService.query = event.currentTarget.elements.query.value;
-  if (newApiService.query === '' || newApiService.query === ' ') {
+    if (newApiService.query === '' || newApiService.query === ' ') {
     clearArticlesContainer();
         return error({
       text: `The search is empty`,
       delay: 2000
         });
-
   };
+
+  // if (newApiService.query !== 'IMG') {
+  //     return;
+  // }
+
+
+
+
+
   newApiService.resetPage();
   newApiService.fetchArticles().then(articles => {
     clearArticlesContainer();
     appendArticlesMarkup(articles);
     show();
+    //console.log(articles.length);
+     if (articles.length < 12) {
+    refs.loadMoreBtn.classList.add('is-hidden');
+  }
+
+
   });
 }
 function onLoadMore(event) {
@@ -46,8 +62,20 @@ function appendArticlesMarkup(articles) {
    refs.gallery.scrollIntoView({
     behavior: 'smooth',
     block: 'end',
-  });
-  };
+   });
+  //console.log(articles.length);
+  // if (articles.length < 12) {
+  //   refs.loadMoreBtn.classList.add('is-hidden');
+  // }
+
+
+
+};
+  // if (articles.length < 12) {
+  //   show();
+  // }
+
+
 
 function clearArticlesContainer() {
   refs.gallery.innerHTML = '';
@@ -60,3 +88,6 @@ function show() {
 function hide() {
   refs.loadMoreBtn.classList.add('is-hidden')
  }
+
+
+
